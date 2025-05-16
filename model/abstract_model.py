5dc364ae669c882312d865fb32a06b63ab13abcb
admin <polivencus@gmail.com>
2025-05-16 10:06:52 +0200

    docstrings added

--- model/abstract_model.py
+++ model/abstract_model.py
@@ -1,16 +1,23 @@
 import torch
 
 class AbstractModel(torch.nn.Module):
+    """This is an abstract class for models.
+
+    Args:
+        torch (_type_): torch base class for all neural networks. From:
+        https://docs.pytorch.org/docs/stable/generated/torch.nn.Module.html
+    """
     def __init__(self):
+        """This is AbstractModel class constructor."""
         super().__init__()
         self._device = "cuda" if torch.cuda.is_available() else "cpu"
 
     def forward(self, x):
-        """This is the forward pass of the model"""
+        """This is the forward pass of the model."""
         pass
     
     @property
     def device(self):
-        """This is a getter of the device"""
+        """This is a getter of the device."""
         return self._device
     
