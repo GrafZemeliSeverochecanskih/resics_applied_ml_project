fixed test accuracy calculation

model/new_model/resnet50_custom_model.py
@@ -37,9 +37,8 @@ class ResNet50CustomModel(AbstractModel):
         self.__val_dir = self.__image_path / val
         self.__test_dir = self.__image_path / test
 
-        self.__train_dataloader, self.__test_dataloader, \
-            self.__val_dataloader , \
-                self.__class_names = self.__initializeDataHandler()
+        _, _, self.__test_dataloader , self.__class_names \
+            = self.__initializeDataHandler()
         
         self.__device = "cuda" if torch.cuda.is_available() else "cpu"
         self.__unfreeze_classifier = unfreeze_classifier
@@ -126,7 +125,8 @@ class ResNet50CustomModel(AbstractModel):
         """This is a getter for the model."""
         return self.__model
 
-    def evaluate_on_test_set(self, test_loader):
+    @property
+    def test_accuracy(self):
         """Evaluates the model on the given test dataset and returns
           accuracy.
         """
@@ -135,7 +135,7 @@ class ResNet50CustomModel(AbstractModel):
         total = 0
 
         with torch.no_grad():
-            for images, labels in self.__test_loader:
+            for images, labels in self.__test_dataloader:
                 images = images.to(self.__device)
                 labels = labels.to(self.__device)
 
@@ -144,7 +144,7 @@ class ResNet50CustomModel(AbstractModel):
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
 
-        accuracy = 100 * correct / total
-        print(f"Accuracy on test set: {accuracy:.2f}%")
-        return accuracy
+        test_accuracy = 100 * correct / total
+        print(f"Accuracy on test set: {test_accuracy:.2f}%")
+        
         

model/upload_model/upload_model.py
@@ -12,6 +12,7 @@ class UploadResNet50Model(AbstractModel):
     """
     def __init__(self,
                  weight_path: str,
+                 image_path: str,
                  dropout_rate: float = 0.5,
                  unfreeze_specific_blocks: list[str] = None,
                  unfreeze_classifier: bool = True
@@ -29,11 +30,13 @@ class UploadResNet50Model(AbstractModel):
         super().__init__()
         self.__device = "cuda" if torch.cuda.is_available() else "cpu"
         self.__model = ResNet50CustomModel(
+            image_path,
             dropout_rate=dropout_rate,
             unfreeze_specific_blocks = unfreeze_specific_blocks,
             unfreeze_classifier = unfreeze_classifier
         ) 
-        self.__model.load_state_dict(torch.load(weight_path, map_location=torch.device(self.__device)))
+        self.__model.load_state_dict(torch.load(weight_path, 
+                                                map_location=torch.device(self.__device), ))
         self.__model.to(self.__device)
         print(f"Model loaded successfully from '{weight_path}' onto '{self.__device}' and set to evaluation mode.")
     
@@ -42,5 +45,5 @@ class UploadResNet50Model(AbstractModel):
         return self.__model
     
     @property
-    def test_accuracy_score(self):
-        return self.__model.evaluate_on_test_set()
+    def test_accuracy(self):
+        return self.__model.test_accuracy